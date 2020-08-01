3ef2bc5491f77193e5b7047dcb2a6f6b3f8d664c
Chethan Prabhu <chethanprabhu65@gmail.com>
2020-08-01 23:07:52 +0530

    added comments

--- states_using_class_based_components/src/App.js
+++ states_using_class_based_components/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import Person from "./Person/Person"
 
 class App extends Component {
+
+    /*state is a reserved keyword. We can assign anything to a state. In this case it's an array of objects. But it can be anything say,
+    a string, a number or a boolean, a function, etc etc anything */
     state = {
         persons: [
             {name: "Chethan", age: 24},
@@ -11,7 +14,12 @@ class App extends Component {
         ],
         whoAreWe: "Best Buddies"
     }
-
+    
+    /*This getFullNameHandler can be any name given by the developer. But it's a good practice to append the word 'handler' at the end
+    Inside this we use the method setState(again it's a reserved keyword). Here inside this function we basically pass the new state that
+    we want to update. Remember if there are any additional states in original state, it won't be altered. Only thing is whatever u pass in 
+    setState gets merged with original state. In this case the original state and setState had same person array. Hence, it just overrided
+    the same object and updated it with these new values. However the 'whoAreWe' would still remain unchanged. */
     getFullNameHandler = () => {
         this.setState(
             {
