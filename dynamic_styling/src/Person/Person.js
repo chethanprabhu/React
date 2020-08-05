ea03eeaa8a3d77945c1366a12e06a2738d88a28c
Chethan Prabhu <chethanprabhu65@gmail.com>
2020-08-05 23:58:19 +0530

    initial setup

---
+++ dynamic_styling/src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Person = (props) => {
+    return (
+        <div>
+            <p onClick={props.onClick}>My name is {props.name} and I am {props.age} years old.</p>
+            {/* onChange runs whenever we change any value in that input */}
+            <input type="text" onChange={props.onEdit} value={props.name}/>
+        </div>
+    )
+}
+
+export default Person;
